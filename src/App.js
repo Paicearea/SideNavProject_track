44dceef0573c3c80b21aeb86ab36c0f7e75a0085
paice <qocodms20010302@gmail.com>
2023-02-23 18:11:53 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import styled, { createGlobalStyle } from "styled-components";
+import Content from "./Content";
+import SideNav2 from "./SideNav2";
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    background-color: #ffffff;
+  }
+`;
+
+const Layout = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 32px 0;
+  color: #a7a9be;
+  font-size: 1.5rem;
+  font-family: sans-serif;
+`;
+
+function App() {
+  return (
+    <Layout>
+      <GlobalStyle />
+      <SideNav2 />
+      <Content />
+    </Layout>
+  );
+}
+
+export default App;
